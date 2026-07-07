c1929190588f027c0648ee9131e4a6510f361483
Adrian3312401110 <adrianrizqullah4@gmail.com>
2026-07-07 21:33:27 +0700

    update seluruh isi folder backend

--- backend/models.py
+++ backend/models.py
@@ -1,27 +1,7 @@
-import os
 from sqlalchemy import Column, String, Integer, Float, DateTime, Boolean, Text, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from backend.database import Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 class UserAccount(Base):
